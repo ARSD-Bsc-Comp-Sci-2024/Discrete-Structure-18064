33b66b7cd4140a61f1680932727d433583326e04
AnanyaaKoundal <104027870+AnanyaaKoundal@users.noreply.github.com>
2022-05-07 19:00:32 +0530

    Linear Search.cpp

---
+++ Linear Search.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Lsearch{
+	int n;
+	int *a;
+	public:
+		Lsearch(int size){
+			n=size;
+			a=new int[n];
+		}
+		void input(){
+			cout<<"\nEnter the elements:";
+			for (int i=0; i<n;i++)
+				cin>>a[i];
+		}
+		int search(int n){
+			for(int i=0; i<n; i++){
+				if(a[i]==n)
+					return i;
+			}
+			return -1;
+		}
+};
+
+int main(){
+	clrscr();
+	int n, find, res;
+	cout<<"Enter the size of the array: ";
+	cin>>n;
+	Lsearch A(n);
+	A.input();
+	cout<<"\nEnter the element to be searchrd: "<<endl;
+	cin>>find;
+	res=A.search(find);
+	if (res<0){
+		cout<<"\nElement not found in the array."<<endl;
+	} else{
+		cout<<"\nElement found at index "<<res<<" in the array."<<endl;
+	}
+	getchar();
+	return  0;
+}
